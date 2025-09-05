37fa153ccd98fdc0145473078f09118ea5c027e5
Ronelle Caguioa <ronelle@google.com>
2025-09-05 13:07:55 -0700

    Cache joint distribution during sequential sampling.
    PiperOrigin-RevId: 803574128

--- meridian/model/posterior_sampler.py
+++ meridian/model/posterior_sampler.py
@@ -83,6 +83,7 @@ class PosteriorMCMCSampler:
 
   def __init__(self, meridian: "model.Meridian"):
     self._meridian = meridian
+    self._joint_dist = None
 
   @property
   def model(self) -> "model.Meridian":
@@ -460,13 +461,15 @@ class PosteriorMCMCSampler:
     return joint_dist_unpinned
 
   def _get_joint_dist(self) -> backend.tfd.Distribution:
-    mmm = self.model
-    y = (
-        backend.where(mmm.holdout_id, 0.0, mmm.kpi_scaled)
-        if mmm.holdout_id is not None
-        else mmm.kpi_scaled
-    )
-    return self._get_joint_dist_unpinned().experimental_pin(y=y)
+    if self._joint_dist is None:
+      mmm = self.model
+      y = (
+          backend.where(mmm.holdout_id, 0.0, mmm.kpi_scaled)
+          if mmm.holdout_id is not None
+          else mmm.kpi_scaled
+      )
+      self._joint_dist = self._get_joint_dist_unpinned().experimental_pin(y=y)
+    return self._joint_dist
 
   def __call__(
       self,
@@ -555,6 +558,9 @@ class PosteriorMCMCSampler:
     seed = backend.random.sanitize_seed(seed) if seed is not None else None
     n_chains_list = [n_chains] if isinstance(n_chains, int) else n_chains
     total_chains = np.sum(n_chains_list)
+
+    # Clear joint distribution cache prior to sampling.
+    self._joint_dist = None
 
     states = []
     traces = []
