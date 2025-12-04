354bcfe9e1db6e432fac0a9dc0464cefd15c5ae4
Lukasz Mazurek <lukmaz@google.com>
2025-12-04 08:42:25 -0800

    [refactor] Move PriorDistribution logic to ModelContext
    PiperOrigin-RevId: 840261116

--- meridian/model/context.py
+++ meridian/model/context.py
@@ -16,6 +16,7 @@
 
 from collections.abc import Mapping
 import functools
+import warnings
 
 from meridian import backend
 from meridian import constants
@@ -23,6 +24,7 @@ from meridian.data import input_data as data
 from meridian.model import adstock_hill
 from meridian.model import knots
 from meridian.model import media
+from meridian.model import prior_distribution
 from meridian.model import spec
 from meridian.model import transformers
 import numpy as np
@@ -48,6 +50,12 @@ class ModelContext:
     self._model_spec = model_spec
 
     self._validate_model_spec_shapes()
+
+    self._set_total_media_contribution_prior = False
+    self._warn_setting_ignored_priors()
+    self._validate_mroi_priors_non_revenue()
+    self._validate_roi_priors_non_revenue()
+    self._check_media_prior_support()
 
   def _validate_model_spec_shapes(self):
     """Validate shapes of model_spec attributes."""
@@ -346,6 +354,244 @@ class ModelContext:
       return None
     tensor = backend.to_tensor(self._model_spec.holdout_id, dtype=backend.bool_)
     return tensor[backend.newaxis, ...] if self.is_national else tensor
+
+  def _warn_setting_ignored_priors(self):
+    """Raises a warning if ignored priors are set."""
+    default_distribution = prior_distribution.PriorDistribution()
+    for ignored_priors_dict, prior_type, prior_type_name in (
+        (
+            constants.IGNORED_PRIORS_MEDIA,
+            self._model_spec.effective_media_prior_type,
+            "media_prior_type",
+        ),
+        (
+            constants.IGNORED_PRIORS_RF,
+            self._model_spec.effective_rf_prior_type,
+            "rf_prior_type",
+        ),
+    ):
+      ignored_custom_priors = []
+      for prior in ignored_priors_dict.get(prior_type, []):
+        self_prior = getattr(self._model_spec.prior, prior)
+        default_prior = getattr(default_distribution, prior)
+        if not prior_distribution.distributions_are_equal(
+            self_prior, default_prior
+        ):
+          ignored_custom_priors.append(prior)
+      if ignored_custom_priors:
+        ignored_priors_str = ", ".join(ignored_custom_priors)
+        warnings.warn(
+            f"Custom prior(s) `{ignored_priors_str}` are ignored when"
+            f' `{prior_type_name}` is set to "{prior_type}".'
+        )
+
+  def _validate_mroi_priors_non_revenue(self):
+    """Validates mroi priors in the non-revenue outcome case."""
+    if (
+        self._input_data.kpi_type == constants.NON_REVENUE
+        and self._input_data.revenue_per_kpi is None
+    ):
+      default_distribution = prior_distribution.PriorDistribution()
+      if (
+          self.n_media_channels > 0
+          and (
+              self._model_spec.effective_media_prior_type
+              == constants.TREATMENT_PRIOR_TYPE_MROI
+          )
+          and prior_distribution.distributions_are_equal(
+              self._model_spec.prior.mroi_m, default_distribution.mroi_m
+          )
+      ):
+        raise ValueError(
+            f"Custom priors should be set on `{constants.MROI_M}` when"
+            ' `media_prior_type` is "mroi", KPI is non-revenue and revenue per'
+            " kpi data is missing."
+        )
+      if (
+          self.n_rf_channels > 0
+          and (
+              self._model_spec.effective_rf_prior_type
+              == constants.TREATMENT_PRIOR_TYPE_MROI
+          )
+          and prior_distribution.distributions_are_equal(
+              self._model_spec.prior.mroi_rf, default_distribution.mroi_rf
+          )
+      ):
+        raise ValueError(
+            f"Custom priors should be set on `{constants.MROI_RF}` when"
+            ' `rf_prior_type` is "mroi", KPI is non-revenue and revenue per kpi'
+            " data is missing."
+        )
+
+  def _validate_roi_priors_non_revenue(self):
+    """Validates roi priors in the non-revenue outcome case."""
+    if (
+        self._input_data.kpi_type == constants.NON_REVENUE
+        and self._input_data.revenue_per_kpi is None
+    ):
+      default_distribution = prior_distribution.PriorDistribution()
+      default_roi_m_used = (
+          self._model_spec.effective_media_prior_type
+          == constants.TREATMENT_PRIOR_TYPE_ROI
+          and prior_distribution.distributions_are_equal(
+              self._model_spec.prior.roi_m, default_distribution.roi_m
+          )
+      )
+      default_roi_rf_used = (
+          self._model_spec.effective_rf_prior_type
+          == constants.TREATMENT_PRIOR_TYPE_ROI
+          and prior_distribution.distributions_are_equal(
+              self._model_spec.prior.roi_rf, default_distribution.roi_rf
+          )
+      )
+      # If ROI priors are used with the default prior distribution for all paid
+      # channels (media and RF), then use the "total paid media contribution
+      # prior" procedure.
+      if (
+          (default_roi_m_used and default_roi_rf_used)
+          or (self.n_media_channels == 0 and default_roi_rf_used)
+          or (self.n_rf_channels == 0 and default_roi_m_used)
+      ):
+        self._set_total_media_contribution_prior = True
+        warnings.warn(
+            "Consider setting custom ROI priors, as kpi_type was specified as"
+            " `non_revenue` with no `revenue_per_kpi` being set. Otherwise, the"
+            " total media contribution prior will be used with"
+            f" `p_mean={constants.P_MEAN}` and `p_sd={constants.P_SD}`. Further"
+            " documentation available at "
+            " https://developers.google.com/meridian/docs/advanced-modeling/unknown-revenue-kpi-custom#set-total-paid-media-contribution-prior",
+        )
+      elif self.n_media_channels > 0 and default_roi_m_used:
+        raise ValueError(
+            f"Custom priors should be set on `{constants.ROI_M}` when"
+            ' `media_prior_type` is "roi", custom priors are assigned on'
+            ' `{constants.ROI_RF}` or `rf_prior_type` is not "roi", KPI is'
+            " non-revenue and revenue per kpi data is missing."
+        )
+      elif self.n_rf_channels > 0 and default_roi_rf_used:
+        raise ValueError(
+            f"Custom priors should be set on `{constants.ROI_RF}` when"
+            ' `rf_prior_type` is "roi", custom priors are assigned on'
+            ' `{constants.ROI_M}` or `media_prior_type` is not "roi", KPI is'
+            " non-revenue and revenue per kpi data is missing."
+        )
+
+  def _check_media_prior_support(self) -> None:
+    """Checks ROI, mROI, and Contribution prior support when random effects are log-normal.
+
+    Priors for ROI, mROI, and Contribution can only have negative support if the
+    random effects follow a normal distribution. This check enforces that priors
+    have non-negative support when random effects follow a log-normal
+    distribution. This check only applies to geo-level models with log-normal
+    random effects since national models do not have random effects.
+    """
+    prior = self._model_spec.prior
+    if self.n_media_channels > 0:
+      self._check_for_negative_support(
+          prior.roi_m,
+          self.media_effects_dist,
+          constants.TREATMENT_PRIOR_TYPE_ROI,
+      )
+      self._check_for_negative_support(
+          prior.mroi_m,
+          self.media_effects_dist,
+          constants.TREATMENT_PRIOR_TYPE_MROI,
+      )
+      self._check_for_negative_support(
+          prior.contribution_m,
+          self.media_effects_dist,
+          constants.TREATMENT_PRIOR_TYPE_CONTRIBUTION,
+      )
+    if self.n_rf_channels > 0:
+      self._check_for_negative_support(
+          prior.roi_rf,
+          self.media_effects_dist,
+          constants.TREATMENT_PRIOR_TYPE_ROI,
+      )
+      self._check_for_negative_support(
+          prior.mroi_rf,
+          self.media_effects_dist,
+          constants.TREATMENT_PRIOR_TYPE_MROI,
+      )
+      self._check_for_negative_support(
+          prior.contribution_rf,
+          self.media_effects_dist,
+          constants.TREATMENT_PRIOR_TYPE_CONTRIBUTION,
+      )
+    if self.n_organic_media_channels > 0:
+      self._check_for_negative_support(
+          prior.contribution_om,
+          self.media_effects_dist,
+          constants.TREATMENT_PRIOR_TYPE_CONTRIBUTION,
+      )
+    if self.n_organic_rf_channels > 0:
+      self._check_for_negative_support(
+          prior.contribution_orf,
+          self.media_effects_dist,
+          constants.TREATMENT_PRIOR_TYPE_CONTRIBUTION,
+      )
+
+  def _check_for_negative_support(
+      self,
+      dist: backend.tfd.Distribution,
+      media_effects_dist: str,
+      prior_type: str,
+  ) -> None:
+    """Checks for negative support in prior distributions.
+
+    When `media_effects_dist` is `MEDIA_EFFECTS_LOG_NORMAL`, prior distributions
+    for media effects must be non-negative. This function raises a ValueError if
+    any part of the distribution's CDF is greater than 0 at 0, indicating some
+    probability mass below zero.
+
+    Args:
+      dist: The distribution to check.
+      media_effects_dist: The type of media effects distribution.
+      prior_type: The prior type that corresponds with current prior under test.
+
+    Raises:
+      ValueError: If the prior distribution has negative support when
+      `media_effects_dist` is `MEDIA_EFFECTS_LOG_NORMAL`.
+    """
+    if (
+        prior_type == self._model_spec.media_prior_type
+        and media_effects_dist == constants.MEDIA_EFFECTS_LOG_NORMAL
+        and np.any(dist.cdf(0) > 0)
+    ):
+      raise ValueError(
+          "Media priors must have non-negative support when"
+          f' `media_effects_dist`="{media_effects_dist}". Found negative prior'
+          f" distribution support for {dist.name}."
+      )
+
+  @functools.cached_property
+  def prior_broadcast(self) -> prior_distribution.PriorDistribution:
+    """Returns broadcasted `PriorDistribution` object."""
+    total_spend = self._input_data.get_total_spend()
+    # Total spend can have 1, 2 or 3 dimensions. Aggregate by channel.
+    if len(total_spend.shape) == 1:
+      # Already aggregated by channel.
+      agg_total_spend = total_spend
+    elif len(total_spend.shape) == 2:
+      agg_total_spend = np.sum(total_spend, axis=(0,))
+    else:
+      agg_total_spend = np.sum(total_spend, axis=(0, 1))
+
+    return self._model_spec.prior.broadcast(
+        n_geos=self.n_geos,
+        n_media_channels=self.n_media_channels,
+        n_rf_channels=self.n_rf_channels,
+        n_organic_media_channels=self.n_organic_media_channels,
+        n_organic_rf_channels=self.n_organic_rf_channels,
+        n_controls=self.n_controls,
+        n_non_media_channels=self.n_non_media_channels,
+        unique_sigma_for_each_geo=self.unique_sigma_for_each_geo,
+        n_knots=self.knot_info.n_knots,
+        is_national=self.is_national,
+        set_total_media_contribution_prior=self._set_total_media_contribution_prior,
+        kpi=np.sum(self._input_data.kpi.values),
+        total_spend=agg_total_spend,
+    )
 
   @functools.cached_property
   def adstock_decay_spec(self) -> adstock_hill.AdstockDecaySpec:
