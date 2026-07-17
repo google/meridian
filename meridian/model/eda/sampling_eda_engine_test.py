75ddf7c8946d0bfd46780c82df8bc17bba5457ff
Rieman Li <riemanli@google.com>
2026-07-17 16:08:11 -0700

    internal
    PiperOrigin-RevId: 949807745

--- meridian/model/eda/sampling_eda_engine_test.py
+++ meridian/model/eda/sampling_eda_engine_test.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from collections.abc import Sequence
 import dataclasses
 import math
 from unittest import mock
